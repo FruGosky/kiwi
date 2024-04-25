23796fa431c4f8d6339aff1bf3855853fbdabf0b
FruGosky <lukaszkomandzik@gmail.com>
2024-04-25 17:38:33 +0200

    feat(shadcn): shadcn init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
 import type { Metadata } from 'next';
+import { Inter as FontSans } from 'next/font/google';
+import { cn } from '@/lib/utils';
+
+const fontSans = FontSans({
+	subsets: ['latin'],
+	variable: '--font-sans',
+});
 
 export const metadata: Metadata = {
 	title: 'Kiwi',
@@ -11,8 +18,15 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body>{children}</body>
+		<html lang="en" suppressHydrationWarning>
+			<body
+				className={cn(
+					'min-h-screen bg-background font-sans antialiased',
+					fontSans.variable
+				)}
+			>
+				{children}
+			</body>
 		</html>
 	);
 }
