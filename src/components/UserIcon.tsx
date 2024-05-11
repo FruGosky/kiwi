36dd498e7eb62d1474aadd83017c43e9fa1dd13e
FruGosky <lukaszkomandzik@gmail.com>
2024-05-11 12:39:58 +0200

    style(UserIcon.tsx): prevent UserIcon jumping when clerk is loading

--- src/components/UserIcon.tsx
+++ src/components/UserIcon.tsx
@@ -1,16 +1,27 @@
-'use client';
-
-import { useUser, UserButton, SignInButton } from '@clerk/nextjs';
+import {
+	ClerkLoading,
+	SignedIn,
+	SignedOut,
+	SignInButton,
+	UserButton,
+} from '@clerk/nextjs';
+import { Skeleton } from './ui/skeleton';
 import { Button } from './ui/button';
 
 export default function UserIcon() {
-	const { isSignedIn } = useUser();
-
-	return isSignedIn ? (
-		<UserButton />
-	) : (
-		<Button asChild>
-			<SignInButton mode="modal" />
-		</Button>
+	return (
+		<div className="flex h-7 w-7 items-center">
+			<ClerkLoading>
+				<Skeleton className="h-7 w-7 rounded-full" />
+			</ClerkLoading>
+			<SignedIn>
+				<UserButton />
+			</SignedIn>
+			<SignedOut>
+				<Button asChild>
+					<SignInButton mode="modal" />
+				</Button>
+			</SignedOut>
+		</div>
 	);
 }
